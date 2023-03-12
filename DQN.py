c81c02610a35d9882c89f659af958f35bd5c6b21
Nico <117078536+n-bzy@users.noreply.github.com>
2023-03-12 22:46:27 +0100

    Added preprocessing

---
+++ DQN.py
@@ -0,0 +1,91 @@
+import tensorflow as tf
+
+
+class DQN(tf.keras.layers.Layer):
+    def __init__(self, num_actions):
+        super().__init__()
+
+        self.num_actions = num_actions
+
+        self.loss = tf.keras.losses.MeanSquaredError() 
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
+
+        self.metrics_list = [tf.keras.metrics.Mean(name="loss")]
+        
+        self.q_net = [tf.keras.layers.Conv2D(filters=32, kernel_size=8, strides = 4, padding='same', activation='relu'), 
+                      tf.keras.layers.TimeDistributed(tf.keras.layers.MaxPool2D()),
+                      tf.keras.layers.Conv2D(filters=64, kernel_size=4, strides = 2, padding='same', activation='relu'),
+                      tf.keras.layers.TimeDistributed(tf.keras.layers.MaxPool2D()),
+                      tf.keras.layers.Conv2D(filters=64, kernel_size=3, padding='same', activation='relu'),
+                      tf.keras.layers.TimeDistributed(tf.keras.layers.GlobalMaxPool2D()),
+                      tf.keras.layers.Dense(512),
+                      tf.keras.layers.Dense(self.num_actions,
+                                            activation=None,
+                                            kernel_initializer=tf.keras.initializers.RandomUniform(minval=-0.03, maxval=0.03),
+                                            bias_initializer=tf.keras.initializers.Constant(-0.2))] #last is q_values layer
+
+    """# Define a helper function to create Dense layers configured with the right
+    # activation and kernel initializer.
+
+    def _dense_layer(self, num_units):
+        return tf.keras.layers.Dense(
+            num_units,
+            activation=tf.keras.activations.relu,
+            kernel_initializer=tf.keras.initializers.VarianceScaling(
+                scale=2.0, mode='fan_in', distribution='truncated_normal'))"""
+    
+    def call(self, x, training = False):
+        """
+        Predicts Q-values (expected rewards) for each action. 
+
+        Parameters:
+            x (ndarray): observation
+            training (boole): enables to train the network 
+
+        Returns: 
+            x (ndarray): Q-values
+        """
+        x = tf.cast(x, tf.float32) / 256.
+        for layer in self.q_net:
+            x = layer(x)
+        return x
+    
+    def reset_metrics(self):
+
+        for metric in self.metrics:
+            metric.reset_states()
+
+    @tf.function
+    def train(self, observation, target): 
+        """
+        Trains the network via backpropagation.
+
+        Parameters: 
+            observation (ndarray): the state of the environment
+            target (float): expected reward from "optimal" action
+        """
+
+        with tf.GradientTape() as tape:
+            predictions = self(observation, training=True)
+            loss = self.loss(target, predictions)
+      
+        gradients = tape.gradient(loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+      
+        # update loss metric
+        self.metrics[0].update_state(loss)
+
+        # Return a dictionary mapping metric names to current value
+        return {m.name: m.result() for m in self.metrics}
+    
+    @tf.function
+    def test(self, observation, target): 
+
+        predictions = self(observation, training=True)
+        loss = self.loss(target, predictions)
+      
+        # update loss metric
+        self.metrics[0].update_state(loss)
+
+        # Return a dictionary mapping metric names to current value
+        return {"val_" + m.name : m.result() for m in self.metrics}
---
+++ training_with_multiple_environments.py
@@ -0,0 +1,79 @@
+import gymnasium as gym
+from Experience_Replay_Buffer import ExperienceReplayBuffer
+from Agent import Agent
+import time
+import numpy as np
+
+
+num_environments = 32 # 32 takes a long time for even one episode. Maybe we should lower that
+num_actions = 6 #env.action_space shouldn't we then also set use_full_action_space=False ?
+m = 50 # amount of training samples
+# timesteps need to be large enough otherwise the agent don't have enough time to play a full game. The for-loop should be stopped by the done flag before this runs out
+timesteps = 5000 # amount of samples to fill ERP with after the its filled up once
+AGGREGATE_STATS_EVERY = 50 # get and safe stats every n episodes
+MIN_REWARD = 0 # safe model only when the lowest reward of model over the last n episodes reaches a threshold
+EPISODES = 20_000
+ERP_size = 100
+MODEL_NAME = "SinglePong" # used for saving and logging
+
+# Exploration settings
+epsilon = 1  # not a constant, going to be decayed
+EPSILON_DECAY = 0.99975
+MIN_EPSILON = 0.001
+
+# instantiate environments
+import gymnasium.wrappers as gw
+
+def create_env():
+   env = gym.make('ALE/Pong-v5') 
+   env = gw.ResizeObservation(env, 84)
+   env = gw.GrayScaleObservation(env, keep_dim=True)
+   env = gw.FrameStack(env, 4)
+   return env
+
+environments = [create_env() for _ in range(num_environments)] #render_mode = 'human')
+
+# instantiate q_network
+Q_net = Agent(num_actions, num_environments, MODEL_NAME)
+Q_net.update_delay_target_network()
+
+ERP = ExperienceReplayBuffer(size = ERP_size)
+
+reward_per_episode = []
+
+for episode in range(EPISODES):
+
+   # do a step function in every Environment, fill the ERP and collect a list of rewards (one for each in the list of environments)
+   reward_of_episode = Q_net.fill(environments, timesteps, ERP, epsilon)
+
+   for amount_of_samples in range(m):
+      sample = ERP.sample()
+      q_target = Q_net.q_target(sample) 
+      observations = [sample[0] for sample in sample]
+      Q_net.network.train(observations, q_target)
+
+   # should we update this every episode?
+   Q_net.update_delay_target_network()
+
+   # I took the average of all environments here that makes it easier to log it. I hope that is okay
+   reward_per_episode.append(np.mean(reward_of_episode))
+
+   # every n episodes this safes the average and min / max rewards of these episodes to the tensorboard
+   if not episode % AGGREGATE_STATS_EVERY or episode == 1:
+      average_reward = sum(reward_per_episode[-AGGREGATE_STATS_EVERY:]) / len(reward_per_episode[-AGGREGATE_STATS_EVERY:])
+      min_reward = min(reward_per_episode[-AGGREGATE_STATS_EVERY:])
+      max_reward = max(reward_per_episode[-AGGREGATE_STATS_EVERY:])
+      Q_net.tensorboard.update_stats(rewards_avg=average_reward, reward_min=min_reward, reward_max=max_reward)
+
+      # Save model, but only when min reward is greater or equal a set value
+      if min_reward >= MIN_REWARD:
+         Q_net.model.save(
+            f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
+
+   # decay epsilon
+   if epsilon > MIN_EPSILON:
+      epsilon *= EPSILON_DECAY
+      epsilon = max(MIN_EPSILON, epsilon)
+
+   # we should consider printing only the average of the rewards
+   print(f'done with epsiode {episode} with reward {reward_of_episode}') 
