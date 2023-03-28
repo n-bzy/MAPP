79acbedeb1d145fec14bfd1dfa31ecf38d8c588d
Nico <117078536+n-bzy@users.noreply.github.com>
2023-03-28 12:41:12 +0200

    Update to create_env()

--- Single_Agent_Pong/training_setup.py
+++ Single_Agent_Pong/training_setup.py
@@ -2,23 +2,25 @@ import gymnasium as gym
 import gymnasium.wrappers as gw
 
 
-def hyperparameter_settings(num_actions = 6, ERP_size = 10000, EPISODES = 20_000, 
+def hyperparameter_settings(num_actions = 6, ERP_size = 75000, EPISODES = 20_000, 
                             epsilon = 1,
                             MODEL_NAME = "SinglePong", AGGREGATE_STATS_EVERY = 1, MIN_REWARD = -21, UPDATE_TARGET_EVERY = 10):
     '''
     Parameters: 
+        num_environments (int): amount of environments
         num_actions (int): amount of possible actions for the agent
         ERP_size (int): size of the experience replay buffer
+        num_training_samples (int): amount of samples the agent trains on every episode
+        TIMESTEPS (int): amount of new samples to fill ERP with
         EPISODES (int): amount of epsiodes to train for
         For exploration:
             epsilon (float): not a constant, going to be decayed
+            EPSILON_DECAY (float): rate to decay epsilon
+            MIN_EPSILON (float): the minimum allowed for epsilon
         For logging:
             MODEL_NAME (str): used for saving and logging
             AGGREGATE_STATS_EVERY (int): get and safe stats every n episodes
-            MIN_REWARD (int): safe model only when the lowest reward of model over the last n episodes reaches a threshold 
-
-        Returns: 
-            All Parameters above
+            MIN_REWARD (int): safe model only when the lowest reward of model over the last n episodes reaches a threshold
     '''
     return num_actions, ERP_size, EPISODES, epsilon, MODEL_NAME, AGGREGATE_STATS_EVERY, MIN_REWARD, UPDATE_TARGET_EVERY
 
@@ -27,15 +29,14 @@ def create_env(name_env = 'ALE/Pong-v5'):
     '''
     Creates and preprocesses the environment to be of size (4, 84, 84, 1). 
     
-    The 1st-dimension represents the 4 frames stacked together. This is for the Agent to get an idea of the direction of the ball.
+    The 1st-dimension represents the 4 frames stacked together, to get an idea of the direction of the ball.
     The 2nd and 3rd dimension represent the size of the observation.
     And the 4th dimension represents the colorchannel which is grayscale.
-
     Return:
         env (gymnasium): an environment to train an agent 
     '''
-    env = gym.make(name_env)
-    env = gw.ResizeObservation(env, 84)
-    env = gw.GrayScaleObservation(env, keep_dim=True)
+
+    env = gym.make(name_env, frameskip=1)
+    env = gw.AtariPreprocessing(env, noop_max=30, frame_skip=4, screen_size=84, terminal_on_life_loss=False, grayscale_obs=True, grayscale_newaxis=True,scale_obs=False)
     env = gw.FrameStack(env, 4)
     return env
