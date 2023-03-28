c97e91ac2d86f3ed690c3b69a81a870258920968
Nico <117078536+n-bzy@users.noreply.github.com>
2023-03-28 12:42:58 +0200

    Correct file

--- Single_Agent_Pong/training_setup.py
+++ Single_Agent_Pong/training_setup.py
@@ -7,20 +7,18 @@ def hyperparameter_settings(num_actions = 6, ERP_size = 75000, EPISODES = 20_000
                             MODEL_NAME = "SinglePong", AGGREGATE_STATS_EVERY = 1, MIN_REWARD = -21, UPDATE_TARGET_EVERY = 10):
     '''
     Parameters: 
-        num_environments (int): amount of environments
         num_actions (int): amount of possible actions for the agent
         ERP_size (int): size of the experience replay buffer
-        num_training_samples (int): amount of samples the agent trains on every episode
-        TIMESTEPS (int): amount of new samples to fill ERP with
         EPISODES (int): amount of epsiodes to train for
         For exploration:
             epsilon (float): not a constant, going to be decayed
-            EPSILON_DECAY (float): rate to decay epsilon
-            MIN_EPSILON (float): the minimum allowed for epsilon
         For logging:
             MODEL_NAME (str): used for saving and logging
             AGGREGATE_STATS_EVERY (int): get and safe stats every n episodes
-            MIN_REWARD (int): safe model only when the lowest reward of model over the last n episodes reaches a threshold
+            MIN_REWARD (int): safe model only when the lowest reward of model over the last n episodes reaches a threshold 
+
+        Returns: 
+            All Parameters above
     '''
     return num_actions, ERP_size, EPISODES, epsilon, MODEL_NAME, AGGREGATE_STATS_EVERY, MIN_REWARD, UPDATE_TARGET_EVERY
 
